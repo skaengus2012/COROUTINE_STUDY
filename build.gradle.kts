4e9af488a3f19c28dd3e52a3f1a64b5e40a2597b
Doohyun <skaengus2012@naver.com>
2020-01-16 19:20:34 +0900

    Implement #0 - Adding JUnit 5

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
+group = "com.nlab.coroutine"
+version = "1.0-SNAPSHOT"
+
 plugins {
     kotlin("jvm") version "1.3.61"
     kotlin("kapt") version "1.3.61"
 }
-
-group = "com.nlab.coroutine"
-version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -17,8 +17,10 @@ dependencies {
     implementation("org.jetbrains.kotlinx", "kotlinx-coroutines-test", "1.3.3")
     testImplementation("org.jetbrains.kotlinx", "kotlinx-coroutines-test","1.3.3")
 
-    // junit
-    testImplementation("junit", "junit", "4.12")
+    // junit 5
+    testImplementation("org.junit.jupiter", "junit-jupiter-api", "5.5.2")
+    testCompile("org.junit.jupiter", "junit-jupiter-params", "5.5.2")
+    testRuntime("org.junit.jupiter", "junit-jupiter-engine", "5.5.2")
 
     // mockito
     testImplementation("org.mockito", "mockito-core", "3.2.4")
@@ -30,5 +32,9 @@ tasks {
     }
     compileTestKotlin {
         kotlinOptions.jvmTarget = "1.8"
+    }
+
+    test {
+        useJUnitPlatform()
     }
 }
